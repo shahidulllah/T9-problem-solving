f1d1c62f8cfc2ce323e2739b836893d84f978c60
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-11 22:53:03 +0600

    object manipulatio

---
+++ ObjectManipulation.js
@@ -0,0 +1,16 @@
+//Create an array of objects representing books with properties like title, author, and year.
+//Write a function that takes the array and returns a new array with only the book titles.
+//Print the result.
+
+const books = [
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
+  { title: "1984", author: "George Orwell", year: 1949 },
+  { title: "To Kill a Mockingbird", author: "Harper Lee", year: 1960 },
+  { title: "Pride and Prejudice", author: "Jane Austen", year: 1813 },
+];
+
+const getBookTittle = (array) => {
+  return array.map((book) => book.title);
+};
+
+console.log(getBookTittle(books));
