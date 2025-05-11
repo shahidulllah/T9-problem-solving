9314c06b83bb6a430721ab19386a8655c899c751
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-11 23:02:03 +0600

    modify

--- ArrayFiltering&Mapping.js
+++ ArrayFiltering&Mapping.js
@@ -1,4 +1,7 @@
-// Task 1: Array Filtering and Mapping
+//Task-1: Array Filtering and Mapping
+/*Create an array of objects, each representing a person with properties like name, age,
+and gender. Write a function to filter out all females and then map the remaining people
+to an array of names. Print the final result.*/
 
 const ArrayOfpeople = [
   { name: "Sabbir", age: 25, gender: "male" },
--- ObjectManipulation.js
+++ ObjectManipulation.js
@@ -1,6 +1,7 @@
-//Create an array of objects representing books with properties like title, author, and year.
-//Write a function that takes the array and returns a new array with only the book titles.
-//Print the result.
+//Task-2: Object Manipulation
+/*Create an array of objects representing books with properties like title, author, and year.
+Write a function that takes the array and returns a new array with only the book titles.
+Print the result.*/
 
 const books = [
   { title: "The Great Gatsby", author: "F. Scott Fitzgerald", year: 1925 },
