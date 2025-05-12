8c7d5a01d06e204d49340153873200a4eb8470ce
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-12 23:39:26 +0600

    Sorting average grade

---
+++ SortingGrade.js
@@ -0,0 +1,23 @@
+//Task-9: Advanced Sorting
+/*Create an array of objects representing students with 'name' and 'grades' properties.
+Write a function to sort the students by average grade in descending order.*/
+
+const students = [
+  { name: "Kulsum", grades: [85, 90, 92] },
+  { name: "Rabbil Hasan", grades: [78, 80, 75] },
+  { name: "Al-Amin", grades: [90, 95, 88] },
+  { name: "Dina", grades: [92, 88, 95] },
+];
+
+function sortByAverageGrade(studentsArray) {
+  return [...studentsArray].sort((a, b) => {
+    const avgA =
+      a.grades.reduce((sum, grade) => sum + grade, 0) / a.grades.length;
+    const avgB =
+      b.grades.reduce((sum, grade) => sum + grade, 0) / b.grades.length;
+    return avgB - avgA;
+  });
+}
+
+const sortedStudents = sortByAverageGrade(students);
+console.log(sortedStudents);
