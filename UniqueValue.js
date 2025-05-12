96ec969d7c93fe4857e90967c316b3e06f9afa2b
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-12 23:29:05 +0600

    unique value

---
+++ UniqueValue.js
@@ -0,0 +1,11 @@
+//Task-7: Unique Values
+/*Create an array of numbers with some duplicate values. Write a function to filter out the
+duplicate values and return a new array with only unique numbers. Print the result.*/
+
+const array = [1, 4, 5, 2, 5, 2, 5, 6, 8, 7, 9];
+
+function uniqueVlaue(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(uniqueVlaue(array));
