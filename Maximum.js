9fde19d00cf954062eef5fe664e813f362aefd4c
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-17 19:57:26 +0600

    modify

--- Maximum.js
+++ Maximum.js
@@ -1,7 +1,7 @@
 //Task-8: Find Maximum Value:
 /*Write a function that takes an array of numbers and returns the maximum value.*/
 
-const array = [1, 4, 5, 2, 5, 2, 5, 6, 8, 7, 9];
+const array = [1, 4, 5, 2, 5, 2, 5];
 
 function maxNum(arr) {
   return Math.max(...arr);
