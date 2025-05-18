5024fbc887ec4cb6e3fe08e255255f1a7c353499
shahidulllah <mdshahidsumon177@gmail.com>
2025-05-18 18:31:29 +0600

    modifying

--- Maximum.js
+++ Maximum.js
@@ -12,5 +12,16 @@ function minimum(arr) {
   return Math.min(arr);
 }
 
-console.log(maxNum(array));
-console.log(maxNum(array));
+// console.log(maxNum(array));
+// console.log(maxNum(array));
+
+//average
+function average (arr){
+  const sum = arr.reduce((s, num)=>{
+   return s + num
+  }, 0)
+
+  return sum/ arr.length 
+}
+
+console.log(average(array));
